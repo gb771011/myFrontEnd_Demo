6d48a5b6a8959575e41b14ed2a442f0b172fab73
gb771011 <gb771011@gmail.com>
2017-04-19 11:27:49 +0800

    Batch Update:17/04/19_11:27

---
+++ git push.bat
@@ -0,0 +1,12 @@
+@echo off
+REM echo 日期:%date%
+REM echo 時間:%time%
+
+set newCommit=Batch Update:%date:~2,8%_%time:~0,5% 
+REM echo %newCommit%
+
+git add .
+git commit -m "%newCommit%"
+git push -u origin master
+
+pause
