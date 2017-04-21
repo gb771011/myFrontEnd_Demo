d6737a9b04658449e6fb553d59906269efd68f2b
gb771011 <gb771011@gmail.com>
2017-04-22 00:11:05 +0800

    Home Update:17/04/22[ 0:11:05]

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function clock(){
         console.log(localeSpilt[1]);
         console.log(stringSpilt[4]);
 
-        document.title=localeSpilt[2];
+        document.title="Now:"+localeSpilt[1];
         
     }, 1000);
 }
