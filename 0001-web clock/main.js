f283ea24cfcd79c52cf5ccf62fcb3558e9efaf80
gb771011 <gb771011@gmail.com>
2017-04-27 17:06:02 +0800

    Batch Update:17/04/27_17:06

--- 0001-web clock/main.js
+++ 0001-web clock/main.js
@@ -1,28 +1,82 @@
-//設定副程式clock顯示當前時間
-function clock(){
-    setInterval(function() {//讓下列程式碼每隔固定時間就執行一次
-        
-        var date=new Date();//建立一date()物件
-        var localeTime=date.toLocaleString();//使用toLocaleString()顯示時間
-        var stringTime=date.toString();//使用toString()顯示時間
-        // 注意上述兩個method的輸出會不同
-
-        document.getElementById("clock").innerHTML="toLocaleString(): "+localeTime;//在網頁的#clock裡顯示localeTime
-        document.getElementById("clock2").innerHTML="toString(): "+stringTime;//在網頁的#clock2裡顯示stringTime
-        
-        // 在console裡檢查輸出時間的陣列內容
-        // [str].spilt(" ")，以" "為分隔符號，將[str]分解為陣列
-        var localeSpilt=localeTime.split(" "); 
-        var stringSpilt=stringTime.split(" ");
-        // 使用console.log顯示陣列結果
-        console.log(stringSpilt);
-        console.log(localeSpilt);
-
-        // 修改網頁的標題
-        document.title=localeSpilt[1];
-        
-    }, 1000);//讓上述程式碼每隔1000ms(1s)就執行一次
+//function clockLocale():顯示當前本地時間
+function showTimeLocale() {
+    //建立一date()物件
+    var date = new Date();
+    // 在網頁的#clock裡顯示date.toLocaleTimeString()
+    document.getElementById("clockL").innerHTML = date.toLocaleTimeString();
 }
 
-//在網頁完成讀取時執行clock()
-window.onload=clock();
+//function clock():顯示當前標準時間
+function showTimeStanard() {
+    var date = new Date();
+    // 在網頁的#clock2裡顯示date.toTimeString()
+    document.getElementById("clockS").innerHTML = date.toTimeString();
+
+    // 注意兩個method的輸出格式不同
+}
+
+var clockL;
+var clockS;
+
+function clockLSwitch(bln){
+    // true=Run,false=stop
+    if(bln){
+        clockL=setInterval(showTimeLocale,1000);
+        console.log("TimeLocale on");
+    }
+    else{
+        window.clearInterval(clockL);
+        console.log("TimeLocale off");
+    }
+}
+
+
+
+
+
+function clock_run() {    
+    clock_on=setInterval(clock,1000);
+    console.log("clock_on");
+}
+
+function clock_pause() {    
+    window.clearInterval(clock_on);
+    console.log("clock_off");
+}
+
+
+
+
+
+
+
+function clock_console(){
+    // 在console裡檢查輸出時間的陣列內容
+    // [str].spilt(" ")，以" "為分隔符號，將[str]分解為陣列
+    var localeSpilt = localeTime.split(" ");
+    var stringSpilt = stringTime.split(" ");
+    // 使用console.log顯示陣列結果
+    console.log(stringSpilt);
+    console.log(localeSpilt);
+
+    // 修改網頁的標題
+    document.title = localeSpilt[1];
+}
+
+var clock_on;
+
+function clock_run() {
+    
+    clock_on=setInterval(clock,1000);
+    console.log("clock_on");
+}
+
+function clock_pause() {
+    
+    window.clearInterval(clock_on);
+    console.log("clock_off");
+}
+
+
+// 在網頁完成讀取時執行clock()
+// window.onload = clock();
