533806ae190a90a22243bcf70578b0a876a89d97
gb771011 <gb771011@gmail.com>
2017-05-16 00:54:42 +0800

    add script2-2

--- 003-calendar/script2-1.js
+++ 003-calendar/script2-1.js
@@ -18,14 +18,13 @@ var myCalendar = document.getElementById("myCalendar");
 
 // 建立新日曆的基本架構:先將所有會用到的元素用innerHTML的方式先加到#myCalendar裡
 myCalendar.innerHTML =
-    '<table><caption id="month">00</caption>' +
+    '<table id="day"><caption id="month">00</caption>' +
     '<tr id="week"></tr>' +
-    '<tr id="day"></tr>' +
     '</table>';
 
 // #week
 // add first table header
-var weekTitle = ["Sun", "Mon", "Tus", "Wed", "Thu", "Fri", "Sat"];
+
 var idWeek = document.getElementById("week");
 var ceTh = document.createElement("th");
 var cont = document.createTextNode(weekTitle[0]);
@@ -50,17 +49,29 @@ var week = [
     ["5", "6", "7", "8", "9", "10", "11"]
 ];
 
-// add first row in table
-var idDay = document.getElementById("day");
-    var ceTr = document.createElement("tr");
-    idDay.appendChild(ceTr);
-for (var i=0;i<7;i++){
-    
-    cont = document.createTextNode(week[0][i]);
-    ceTh.appendChild(cont);
-    idDay.lastChild.appendChild(ceTh);
+console.log(myCalendar);
 
-}
+
+
+// add first row in table
+
+
+/*
+var ceTr = document.createElement("tr");
+idDay.appendChild(ceTr);
+    
+idDay.appendChild(ceTr);
+
+//add first data in new row
+var ceTd = document.createElement("td");
+cont = document.createTextNode(week[0][0]);
+ceTd.appendChild(cont);
+idDay.lastChild.appendChild(ceTd);
+
+ */
+
+
+
 
 
 // var ceTd = document.createElement("th");
