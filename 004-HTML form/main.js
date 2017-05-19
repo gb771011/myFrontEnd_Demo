dd61bc84215523b7a20cf9a3b256af28268832e0
gb771011 <gb771011@gmail.com>
2017-05-19 17:35:26 +0800

    0519 offwork

--- 004-HTML form/main.js
+++ 004-HTML form/main.js
@@ -1,4 +1,30 @@
-function showit() {
-    alert("hello world");
+
+
+/**
+
+
+for (i = 0; i < nP.length; i++) {
+    if (nP[i].parentElement.nodeName === "FORM") {
+        iValue=nP[i].children[0].attributes[0].value;
+
+        tpContent = document.createTextNode(iValue+" : ");
+        tpSpan = document.createElement("span");
+        tpSpan.appendChild(tpContent);
+        // console.log(tpSpan);
+        nP[i].insertBefore(tpSpan,nP[i].firstChild);
+    }
 }
 
+ */
+var i, tpContent, tpSpan,iValue,
+    tagInput = document.getElementsByTagName("input");
+//在所有<input>前面加上type
+
+// console.log(tagInput);
+console.dir(tagInput[0]);
+console.dir(tagInput[1]);
+
+tpContent=document.createTextNode("addtest");
+tpSpan=document.createElement("span");
+
+tpSpan.appendChild(tpContent);
