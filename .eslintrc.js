89e75576f3fdf5d6d076924d027d9e740e8a9f95
gb771011 <gb771011@gmail.com>
2017-09-11 23:09:26 +0800

    layout1 update

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         'no-console':0,
         "indent": [
             "error",
-            "4"
+            4
         ],
         "linebreak-style": [
             "error",
--- layout1/gulpfile.js
+++ layout1/gulpfile.js
@@ -1,43 +1,43 @@
-const path = require("path"),
+const path = require('path'),
     gulp = require('gulp'),
     // 接收外部引數
     gutil = require('gulp-util'),
     // LESS compiler
     less = require('gulp-less'),
     // browser-sync
-    bs = require("browser-sync").create();
+    bs = require('browser-sync').create();
 let root = process.cwd();
 let file = {
-    "less": "style.less"
+    'less': 'style.less'
 };
 
 // 確認變數
-gulp.task("cwd", () => {
+gulp.task('cwd', () => {
     console.log(path.join(root, file.less));
 });
 
 // 轉換less為css
-gulp.task("less", () => {
+gulp.task('less', () => {
     gulp.src(path.join(root, file.less))
         .pipe(less())
         .pipe(gulp.dest(root));
 });
 
 // 檔案監測
-gulp.task("watch", function () {
-    console.log('gulp-watch');
-    // 當less有更動時自動轉存為css
-    gulp.watch("./*.less", ["less"]);
+gulp.task('browerSync', ['less'], () => {
+    // console.log('gulp-watch');
     // 自動重刷頁面
     bs.init({
-        'files':['./**'],
-        'server':{
-            'baseDir':'./'
+        'files': ['./**'],
+        'server': {
+            'baseDir': './'
         },
-        'port':8080
+        'port': 8080,
+        'reloadDelay':1000,
     });
+    // ./*.less有更動時自動轉存為同名的css
+    gulp.watch('./*.less', ['less']);
 });
 
-
 // 預設
-gulp.task("default", ["watch"]);
+gulp.task('default', ['browerSync']);
